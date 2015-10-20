2aa08bff9f911b87f898e48e21d46f27edf34d16
Nattefrost <escande.d@gmail.com>
2015-10-20 19:26:59 +0200

    Funky colours in install.ps1

--- install.ps1
+++ install.ps1
@@ -4,13 +4,13 @@ If (Test-Path "$scriptPath\start.bat")
 	Remove-Item "$scriptPath\start.bat"
 }
 
-echo "=========================================================="
-echo "Downloading Python language"
-echo "When the installer pops up, follow instructions and be sure to check 'add python to PATH'"
-echo "It may take a while..."
-echo "=========================================================="
+write-host "==========================================================" -background "darkblue"
+write-host "Downloading Python language"                                -foreground "green"
+write-host "When the installer pops up, follow instructions and be sure to check 'add python to PATH'" -foreground "green"
+write-host "It may take a while..."                                     -foreground "yellow" -background "DarkRed"
+write-host "==========================================================" -background "darkblue"
 
-<# downloading python3.4.2 installer from python.org
+# downloading python3.4.2 installer from python.org
 $url = "https://www.python.org/ftp/python/3.4.2/python-3.4.2.msi"
 $output = "$scriptPath\python3.msi"
 $start_time = Get-Date
@@ -19,7 +19,7 @@ $wc = New-Object System.Net.WebClient
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-#>
+
 
 # installing python3 and the livestreamer package through the packet manager
 Start-Process .\python3.msi -Wait
@@ -36,8 +36,8 @@ $cmd | out-file "./start.bat" -Encoding ascii
 $shortcut.TargetPath = "$scriptPath\start.bat"
 $shortcut.IconLocation = "$scriptPath\ico.ico"
 $shortcut.Save()
-echo "=========================================================="
-echo "Install over, double-click start.bat on your desktop"
-echo "=========================================================="
+write-host "=========================================================="-background "darkblue"
+write-host "Install over, double-click start.bat on your desktop" -foreground "magenta"
+write-host "==========================================================" -background "darkblue"
 $HOST.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | OUT-NULL
 $HOST.UI.RawUI.Flushinputbuffer()
