434930d7e292ce7b6b54741e6cbe616af8246c2f
Nattefrost <escande.d@gmail.com>
2015-10-20 14:50:14 +0200

    Changed README.MD and have a (hopefully) working version of install.ps1

--- install.ps1
+++ install.ps1
@@ -4,21 +4,28 @@ If (Test-Path "$scriptPath\start.bat")
 	Remove-Item "$scriptPath\start.bat"
 }
 
+echo "=========================================================="
 echo "Downloading Python language"
 echo "When the installer pops up, follow instructions and be sure to check 'add python to PATH'"
 echo "It may take a while..."
+echo "=========================================================="
 
-
-$webclient = New-Object System.Net.WebClient
+<# downloading python3.4.2 installer from python.org
 $url = "https://www.python.org/ftp/python/3.4.2/python-3.4.2.msi"
-$file = "./python3.msi"
-$webclient.DownloadFile($url,$file)
+$output = "$scriptPath\python3.msi"
+$start_time = Get-Date
 
-Start-Process .\python3.msi | out-null
-#Start-Sleep -s 120
+$wc = New-Object System.Net.WebClient
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+#>
+
+# installing python3 and the livestreamer package through the packet manager
+Start-Process .\python3.msi -Wait
 & pip install livestreamer
-echo "Install over, launch start.bat"
 
+# Creating symlink on desktop
 $shell = New-Object -ComObject WScript.Shell
 $desktop = [System.Environment]::GetFolderPath('Desktop')
 $shortcut = $shell.CreateShortcut("$desktop\livestreamer_manager.lnk")
@@ -29,3 +36,8 @@ $cmd | out-file "./start.bat" -Encoding ascii
 $shortcut.TargetPath = "$scriptPath\start.bat"
 $shortcut.IconLocation = "$scriptPath\ico.ico"
 $shortcut.Save()
+echo "=========================================================="
+echo "Install over, double-click start.bat on your desktop"
+echo "=========================================================="
+$HOST.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | OUT-NULL
+$HOST.UI.RawUI.Flushinputbuffer()
